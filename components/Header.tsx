48fbfeb65742be067c4aa37a8b23f87dc0f44450
takumi-333 <takumi_soccer0514@icloud.com>
2024-07-01 11:00:42 +0900

    [add] Headerの作成
    - Headerコンポーネントの作成 (components/Header.tsx)
    - Headerコンポーネントの配置 (app/layout.tsx)
    
    - タイトルをnew qiitaからmy qiitaへ変更しました

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className="fixed z-50 w-screen border-b">
+            <div className="container flex flex-row justify-between px-2 py-2 items-center">
+                <Link href="/" className="md:text-2xl sm:text-xl flex flex-row items-center justify-center">
+                    My Qiita
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header
