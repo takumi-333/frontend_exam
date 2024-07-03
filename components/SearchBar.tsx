e4df4abc494fb9a7faf28202e5bcb75febe6095b
takumi-333 <takumi_soccer0514@icloud.com>
2024-07-03 14:06:11 +0900

    [update] 検索機能をキーワード検索、ユーザID検索に更新しました
    キーワード検索では, 入力されたキーワードがタイトルまたは内容に含まれているものを返します
    ユーザID検索では, 入力されたIDのユーザの投稿を返します
    両方入力された場合は, AND検索となります

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,30 +3,43 @@
 import { Query } from "@/type";
 import React, { ChangeEvent, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
+import { Input } from "./ui/input";
 
 const SearchBar = ( { onSearch } : { onSearch : ( query : Query) => void}) => {
-    const [title, setTitle] = useState<string>("");
+    const [keyword, setKeyword] = useState<string>("");
+    const [userId, setUserId] = useState<string>("");
 
-    const handleTitleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.target.value);
+    const handleKeywordInputChange = (e:ChangeEvent<HTMLInputElement>) => {
+        setKeyword(e.target.value);
+    }
+
+    const handleUserIdInputChange = (e:ChangeEvent<HTMLInputElement>) => {
+        setUserId(e.target.value);
     }
 
     const handleSearch = () => {
         const query: Query = {
             numPage: 1,
-            title: title?title:undefined,
+            title: keyword?keyword:undefined,
+            body: keyword?keyword:undefined,
+            userId:userId?userId:undefined,
         }
         onSearch(query)
     }
 
     return (
         <div className="w-full flex flex-raw items-center gap-2">
-            <input 
-                className="placeholder:text-gray-500 border border-gray-200 outline-2 text-sm rounded-sm w-1/3 pl-1"
+            <Input
                 type="text" 
-                value={title}
-                onChange={handleTitleInputChange} 
-                placeholder="タイトルから検索"
+                value={keyword}
+                onChange={handleKeywordInputChange} 
+                placeholder="キーワードで検索"
+            />
+            <Input
+                type="text"
+                value={userId}
+                onChange={handleUserIdInputChange}
+                placeholder="ユーザIDで検索"
             />
             <button onClick={handleSearch} className="bg-gray-200 hover:bg-gray-300 rounded-sm lg:text-2xl text-xl"><IoIosSearch/></button>
         </div>
